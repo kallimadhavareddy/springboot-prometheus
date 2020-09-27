c73176db537ec20a7b7ccd1125e9d8fd0c88e663
kallimadhavareddy <kalli.madhavareddy@gmail.com>
2020-09-28 00:07:23 +0200

    How to use prometheus in spring boot First Commit

---
+++ src/main/java/com/promtheus/PrometheusDemoApplication.java
@@ -0,0 +1,21 @@
+package com.promtheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+public class PrometheusDemoApplication {
+
+	@GetMapping("hello")
+	public String helloEndPoint(){
+		return "hello this is first api";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promtheus/PrometheusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promtheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
